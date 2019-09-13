a1bc98d6707c912743cee2805e741105995f69d2
Mitchell Dill <malexanderdill@gmail.com>
2019-09-12 22:53:33 -0500

    Init project with react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client');
+const DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: SRC_DIR,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
