f736e22da538af6ee0f18f2b53ccf0850af7e41a
Mitchell Dill <malexanderdill@gmail.com>
2019-09-13 14:04:20 -0500

    Progress on dark sky call for moon data
    Need to move this call to a server, not client

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import Moon from "./moon.jsx";
+import { DARK_SKY_KEY, LATITUDE, LONGITUDE } from "../../api.config.json";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      exists: true,
-      lunationNumber: 0.25,
+      lunationNumber: 0,
+      moonAPIaddress: "https://api.darksky.net/forecast",
+      mercuryAPIaddress: "https://mercuryretrogradeapi.com",
     };
+  }
+
+  async callMoonAPI(date = new Date()) {
+    fetch(
+      `${this.state.moonAPIaddress}/${DARK_SKY_KEY}/${LATITUDE},${LONGITUDE}`,
+      {
+        mode: "no-cors",
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then(response => {
+        response.json();
+      })
+      .then(jsonResponse => {
+        console.log(JSON.stringify(jsonResponse));
+      });
+  }
+
+  componentDidMount() {
+    this.callMoonAPI();
   }
 
   render() {
