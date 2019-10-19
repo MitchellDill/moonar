67d091e78337132c3d47c627ff1ecdbf8f2cb2a9
Mitchell Dill <malexanderdill@gmail.com>
2019-10-19 14:46:11 -0500

    Begin using file loader properly

--- client/components/moonDisplay.jsx
+++ client/components/moonDisplay.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import Zodiac from "./zodiac.jsx";
 import styles from "../style/main.less";
+import phaseImg from "../images/crescentMoon.png";
 
 const moonDisplay = ({ phase }) => {
   return (
     <div>
       <h1>{`${phase}`}</h1>
+      <img className={styles["moonPhase"]} src={phaseImg} />
       {phase === "new" || phase === "full" ? <Zodiac {...props} /> : null}
     </div>
   );
