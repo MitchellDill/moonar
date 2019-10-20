86aa2e56c84a1325c0d4c95fa4621686ec071a44
Mitchell Dill <malexanderdill@gmail.com>
2019-10-20 15:32:21 -0500

    Implement weird modal!

--- client/components/about.jsx
+++ client/components/about.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styles from "../style/main.less";
 
 const About = ({ hideModal }) => {
   return (
@@ -28,26 +29,43 @@ const About = ({ hideModal }) => {
         <a href="https://github.com/MitchellDill/moonar">readme</a> on github,
         but the gist is that moonar makes a call for the lunar data it needs for
         this month and next month, which is stored in a mongoDB collection, and
-        draws the necessary conclusions. The data is derived from lunation
-        numbers supplied by Dark Sky, so per their wishes we are most assuredly{" "}
+        draws the necessary conclusions in its front end. The data is derived
+        from lunation numbers supplied by Dark Sky, so I would be remiss not to
+        mention that we're{" "}
         <a href="https://darksky.net/poweredby/">Powered By Dark Sky</a>{" "}
-        {`\u2122`}. Whenever a month's data isn't found, e.g. we roll over to
-        November and we don't yet have December's info, moonar makes a full
-        month's worth of external API calls to Dark Sky, then stores that info
-        as a new document in the database for future reference. Ideally, because
-        it works a month ahead, we'll never be waiting on those calls when we
-        visit the site. If literally nobody visits for several months, though,
-        we might have to endure a five or six second waiting period as we ring
-        up the fine folks at Dark Sky.
+        {`\u2122`}. Whenever a given month's lunation data isn't found, e.g. we
+        roll over to November and we don't yet have December's info, moonar
+        makes a full month's worth of external API calls to Dark Sky, then
+        stores that info as a new document in the database for future reference.
+        Ideally, because it works a month ahead, we'll never be waiting on those
+        calls when we visit the site. If literally nobody visits for several
+        months, though, we might have to endure a good six or seven second's
+        worth of waiting as we ring up the fine folks at Dark Sky.
       </p>
       <h3>what if i have questions beyond the scope of this about page?</h3>
-      <p>well you could email me i guess.</p>
+      <p>
+        well you could{" "}
+        <a
+          href="mailto:malexanderdill@gmail.com"
+          title="malexanderdill@gmail.com"
+        >
+          email me
+        </a>{" "}
+        i guess.
+      </p>
       <h3>you guess?</h3>
       <p>no, please, i'd love to hear from you.</p>
+      <h3>
+        This to a person who has been nothing but nice about this weird modal
+        you're making me use.
+      </h3>
+      <p>That's fair; it's a bit much. But sometimes you just do a thing.</p>
       <h3
         onClick={e => {
           hideModal(e);
         }}
+        title="click here to close the about page"
+        className={styles["clickableText"]}
       >
         i would like to close this window, please.
       </h3>
